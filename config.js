0e42ac1333e783da7db1d7a923ce5536855dbcf1
Yaham12345 <m44266904@gmail.com>
2024-10-03 22:33:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'afgSTTYB#I3Ev-jTb8lHTBbEheN2wU9aQ82mgpnt9qupDovf0etk', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
     BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
